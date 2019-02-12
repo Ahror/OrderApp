a4e8bb0948e3f241a3d0427edcd3a906076ca97a
Raufjon <raufjonaliboev77@gmail.com>
2019-02-12 14:46:11 +0500

    paymentdetails

--- OrderApp/DateProvider/OrderAppContext.cs
+++ OrderApp/DateProvider/OrderAppContext.cs
@@ -12,13 +12,38 @@ namespace OrderApp.DateProvider
     {
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-          
             
+
+
         }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Menu> Menus { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Payment> Payments { get; set; }
+        public DbSet<PaymentDetail > PaymentDetails { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
