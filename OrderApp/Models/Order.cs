c8af073e00267b94b7c7f21561284bbba9d243b4
Navruz <Navruz@DESKTOP-K250SG6>
2019-02-12 08:41:39 +0500

    create class payment

---
+++ OrderApp/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApp.Models
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public DateTime Date { get; set; }
+        public string Food { get; set; }
+        public Guid Id { get; set; }
+        public bool Closed { get; set; }
+        public string Prise { get; set; }
+        public int PeopleCount { get; set; }
+
+    }
+}
---
+++ OrderApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApp.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public int Ows { get; set; }
+    }
+}
