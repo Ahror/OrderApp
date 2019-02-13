21ba662945e90f1250d95d9ae49ae71711440805
Navruz <Navruz@DESKTOP-K250SG6>
2019-02-13 13:50:17 +0500

    Mapping

--- orderApp/Models/User.cs
+++ orderApp/Models/User.cs
@@ -7,8 +7,25 @@ namespace OrderApp.Models
 {
     public class User
     {
+        public Guid Id
+        {
+            get
+            {
+                if (Id == null || Id == Guid.Empty)
+                {
+                    Id = new Guid();
+                }
+                return Id;
+            }
+            set
+            {
+                Id = value;
+            }
+        }
         public string Login { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
+        public List<Payment> Payments { get; set; }
+        public List<OrderDetail> Details { get; set; }
     }
 }
