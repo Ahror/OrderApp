b004208aadf7d0e8dca9c213a9013cd46b0a7547
Navruz <Navruz@DESKTOP-K250SG6>
2019-02-14 12:57:28 +0500

    Created controllers

--- OrderApp/Startup.cs
+++ OrderApp/Startup.cs
@@ -31,7 +31,7 @@ namespace OrderApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseMvc();
         }
     }
 }
--- orderApp/Models/PaymentDetail.cs
+++ orderApp/Models/PaymentDetail.cs
@@ -7,8 +7,24 @@ namespace OrderApp.Models
 {
     public class PaymentDetail
     {
-        public int DetailId { get; set; }
-        public int OrderId { get; set; } 
+        public Guid Id
+        {
+            get
+            {
+                if (Id == null || Id == Guid.Empty)
+                {
+                    Id = new Guid();
+                }
+                return Id;
+            }
+            set
+            {
+                Id = value;
+            }
+        }
 
+
+        public Payment Payment { get; set; }
+        public OrderDetail OrderDetail { get; set; }
     }
 }
